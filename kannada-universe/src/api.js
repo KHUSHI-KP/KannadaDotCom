f0e84c7af81e9c6247a76e4b6eee3b31c6586de8
krinishita-1602 <krinishita@gmail.com>
2026-04-17 11:36:02 +0530

    Import KannadaGreeting and ThemeSwitcher components

--- kannada-universe/src/api.js
+++ kannada-universe/src/api.js
@@ -1,3 +1,5 @@
+import KannadaGreeting from "./components/KannadaGreeting";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 import axios from "axios";
 
 // Use Vite env variables (import.meta.env) in browser. Falls back to localhost mock server.
